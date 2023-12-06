5308c391dcd1291ac6c67783c9033c5f001e57cf
Kaiwen Yang <92202424+kai-wen-yang@users.noreply.github.com>
2023-12-06 20:32:54 +0800

    Update vqa_ours.sh

--- scripts/vqa_ours.sh
+++ scripts/vqa_ours.sh
@@ -4,4 +4,4 @@ cd /QVix/models
 export PYTHONPATH="$PYTHONPATH:$PWD"
 cd ..
 
-python tools/eval.py --model_name InstructBLIP7B --batch_size 4 --dataset_name ScienceQA --device 0 --expname 'QVix' --sample_num 1000 --task_name vqa_gpt --prompt prompt_hand_v1 --cot "Question: {}Answer:" --api_key ' '
+python tools/eval.py --model_name InstructBLIP7B --batch_size 4 --dataset_name ScienceQA --device 0 --expname 'QVix' --sample_num 1000 --task_name vqa_gpt --prompt prompt_hand_v1 --question "Question: {}Answer:" --api_key ' '
